e26b5227407de229d28757ad567b9d115f1edf28
Castieler <1083298593@qq.com>
2018-03-23 11:56:44 +0800

    修改脚本

--- wenman/jiaoben.py
+++ wenman/jiaoben.py
@@ -24,23 +24,21 @@ num =0
 # for (root, dirs, files) in os.walk(r"/Users/js/Desktop/haodaifu/wenman/wenman/file"):
 #     for filename in files:
 with open(r'/Users/js/Desktop/haodaifu/wenman/wenman/file/hospital.txt','r') as f:
-    list_1 = f.readline()
+    list_1 = f.readlines()
     print(len(list_1))
     for i in list_1:
         print(i)
+        one_data = eval(i)
+        print(one_data)
         print('-------')
-    #     content_list = i.split('|+|')
-    #     print(content_list)
-    #     print(len(content_list))
-        # for each in content_list:
-        # if len(content_list) >= 6:
-        #     sheet.write(num, 0, content_list[0])  # 其中的'0-行, 0-列'指定表中的单元，'EnglishName'是向该单元写入的内容
-        #     sheet.write(num, 1, content_list[1])
-        #     sheet.write(num, 2, content_list[2])  # 其中的'0-行, 0-列'指定表中的单元，'EnglishName'是向该单元写入的内容
-        #     sheet.write(num, 3, content_list[3])
-        #     sheet.write(num, 4, content_list[4])  # 其中的'0-行, 0-列'指定表中的单元，'EnglishName'是向该单元写入的内容
-        #     sheet.write(num, 5, content_list[5])
-        #     num += 1
+
+        sheet.write(num, 0, one_data['hospital_name'])  # 其中的'0-行, 0-列'指定表中的单元，'EnglishName'是向该单元写入的内容
+        sheet.write(num, 1, one_data['hospital_address'])
+        sheet.write(num, 2, one_data['hospital_phone'])  # 其中的'0-行, 0-列'指定表中的单元，'EnglishName'是向该单元写入的内容
+        sheet.write(num, 3, one_data['hospital_level'])
+        sheet.write(num, 4, one_data['hospital_type'])  # 其中的'0-行, 0-列'指定表中的单元，'EnglishName'是向该单元写入的内容
+        sheet.write(num, 5, one_data['hospital_intro'])
+        num += 1
 
 
 book.save(r'/Users/js/Desktop/haodaifu/wenman/wenman/file/hospital.xls')  # 在字符串前加r，声明为raw字符串，这样就不会处理其中的转义了。否则，可能会报错
